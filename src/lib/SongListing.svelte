c1a96127af6b5848b71d171e2c979e2840a29420
Daniel Melero <daniel.melero.001@gmail.com>
2025-02-04 21:40:33 +0100

    Store components in lib + UI to add new song

---
+++ src/lib/SongListing.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let { song } = $props();
+</script>
+
+<div class="flex min-w-0 gap-x-4 ml-4">
+    <div class="min-w-0 flex-auto">
+        <p class="text-sm/6 font-semibold text-gray-900">{song.title}</p>
+        <p class="mt-1 truncate text-xs/5 text-gray-500">{song.author}</p>
+    </div>
+</div>
+<div class="shrink-0 sm:flex sm:flex-col sm:items-end">
+    <audio id="{song.id}" controls>
+        <source src="/{song.id}.mp3" type="audio/mpeg">
+    </audio>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import AddSongButton from '$lib/AddSongButton.svelte';
+    import SongListing from '$lib/SongListing.svelte';
     import songs from '../static/songs.json'; 
 </script>
 
@@ -6,18 +8,12 @@
     <ul role="list" class="divide-y divide-gray-100">
         {#each songs as song}
             <li class="flex justify-between gap-x-6 py-5 rounded-3xl hover:bg-gray-100">
-                <div class="flex min-w-0 gap-x-4 ml-4">
-                    <div class="min-w-0 flex-auto">
-                        <p class="text-sm/6 font-semibold text-gray-900">{song.title}</p>
-                        <p class="mt-1 truncate text-xs/5 text-gray-500">{song.author}</p>
-                    </div>
-                </div>
-                <div class="shrink-0 sm:flex sm:flex-col sm:items-end">
-                    <audio id="{song.id}" controls>
-                        <source src="/{song.id}.mp3" type="audio/mpeg">
-                    </audio>
-                </div>
+                <SongListing {song}/>
             </li>
         {/each}
+
+        <li class="gap-x-6 py-5">
+            <AddSongButton />
+        </li>
     </ul>
 </div>
