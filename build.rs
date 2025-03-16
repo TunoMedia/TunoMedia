e93f46d58435a02df613feaa2c2e14f3aab518e0
Daniel Melero <daniel.melero.001@gmail.com>
2025-03-16 22:56:10 +0100

    Use tuno submodule for its protobuf file

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/tuno.proto")?;
+    tonic_build::compile_protos("tuno/proto/tuno.proto")?;
     Ok(())
 }
