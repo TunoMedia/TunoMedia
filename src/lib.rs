e2893ab4a2635909d30345900d2afc3adee95154
Daniel Melero <daniel.melero.001@gmail.com>
2025-04-05 14:34:41 +0200

    Updated dependencies & added IOTA sdk

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
+use iota_sdk::IotaClientBuilder;
+use anyhow::Result;
+
 pub mod config;
 pub mod server;
 
-use anyhow::Result;
-
 pub async fn run() -> Result<()> {
     let config = config::load_config()?;
+
+    // IOTA testnet running at `https://api.testnet.iota.cafe`
+    let iota_testnet = IotaClientBuilder::default().build_testnet().await?;
+    println!("IOTA testnet version: {:?}", iota_testnet.api_version());
 
     let server = server::TunoGrpcServer::new(
         config.server.host,
