d97cf710b1904e8933f84e7b9692a121c8645897
Daniel Melero <daniel.melero.001@gmail.com>
2025-04-10 18:17:12 +0200

    fix display of song lists

--- src/displays/song_listing.rs
+++ src/displays/song_listing.rs
@@ -1,4 +1,4 @@
-use iota_sdk::rpc_types::IotaMoveStruct;
+use iota_sdk::rpc_types::{IotaMoveStruct, IotaMoveValue};
 use tabled::{Table, Tabled};
 use std::fmt::{Display, Formatter};
 
@@ -8,19 +8,36 @@ pub struct SongListing {
     title: String,
     artist: String,
     is_available: bool,
-    distributors: u64
+    distributors: usize
 }
 
 impl From<IotaMoveStruct> for SongListing {
     fn from(s: IotaMoveStruct) -> Self {
         Self {
-            id: s.read_dynamic_field_value("id").unwrap().to_string(),
-            title: s.read_dynamic_field_value("title").unwrap().to_string(),
-            artist: s.read_dynamic_field_value("artist").unwrap().to_string(),
-            // TODO: Test unavailability
-            is_available: s.read_dynamic_field_value("display_id").is_some(),
-            // TODO: Implement (Might need Display function on smart contract)
-            distributors: 0
+            id: match s.read_dynamic_field_value("id") {
+                Some(IotaMoveValue::UID { id }) => id.to_string(),
+                _ => unreachable!("Error parsing id")
+            },
+            title: match s.read_dynamic_field_value("title") {
+                Some(IotaMoveValue::String(title)) => title,
+                _ => unreachable!("Error parsing title")
+            },
+            artist: match s.read_dynamic_field_value("artist") {
+                Some(IotaMoveValue::String(artist)) => artist,
+                _ => unreachable!("Error parsing artist")
+            },
+            is_available: match s.read_dynamic_field_value("display_id") {
+                Some(IotaMoveValue::Address(_)) => true,
+                Some(IotaMoveValue::Option(_)) => false,
+                _ => unreachable!("Error parsing display_id")
+            },
+            distributors: match s.read_dynamic_field_value("distributors") {
+                Some(IotaMoveValue::Struct(obj)) => match obj.read_dynamic_field_value("contents") {
+                    Some(IotaMoveValue::Vector(distributors)) => distributors.len(),
+                    _ => unreachable!("Error parsing distributors value")
+                }
+                _ => unreachable!("Error parsing distributors vector")
+            }
         }
     }
 }
